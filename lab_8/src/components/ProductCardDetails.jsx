90a9c545e19e03fd68675c6b58d1df627f5e97df
Roman Novachok <romanihorovuch@gmail.com>
2024-11-11 05:08:55 +0200

    add styles to the item page

--- lab_8/src/components/ProductCardDetails.jsx
+++ lab_8/src/components/ProductCardDetails.jsx
@@ -7,6 +7,16 @@ export function ProductCardDetails() {
     
     const {productId} = useParams()
     const [product, setProduct] = useState(null)
+    const [message, setMessage] = useState(null)
+
+    function ShowMessage() {
+        setMessage("Equipment successfully added to cart!")
+
+        setTimeout(() => {
+            setMessage(null);
+        }, 3000);
+        
+    }
 
     useEffect(() => {
         // Знаходимо продукт за його ID з `products.json`
@@ -19,13 +29,30 @@ export function ProductCardDetails() {
     }
 
     return(
+        <div className="product-card-details-content">
+        <div className="message-about-add-to-cart">
+        {message&&
+        <p style={{
+            padding: "5px 10px",
+            fontSize: "25px",
+            color: "rgb(100,100,100, 0.3)",
+            backgroundColor: "rgb(215,215,215, 0.7)",
+            borderRadius: "5px"
+        }}>{message}</p>
+        }
+        </div>
         <div className="ProductCardDetails">
-            <img src={product.image} alt={product.name} />
-            <h1>{product.name}</h1>
-            <p>{product.description}</p>
-            <p>{product.price}</p>
-            <p>{product.type}</p>
+            <div className="product-card-details-image"><img src={product.image} alt={product.name} /></div>
+            <div className="product-card-details-right-area">
+                <h1>{product.name}</h1>
+                <p>Category: {product.type}</p>
+                <p>{product.description}</p>
+                <p>Price: {product.price} грн</p>
+                <button onClick={() => {ShowMessage()}}>Add to cart</button>
+            </div>
 
         </div>
+        </div>
+
     )
 }
